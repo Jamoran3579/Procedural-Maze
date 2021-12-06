b214d1ca244aea6dc943f7e7fe34995a2cb51048
Jamoran3579 <43176942+Jamoran3579@users.noreply.github.com>
2021-12-06 22:13:07 +0000

    Adding code to make the camera move

--- Maze/Assets/LevelGenerator.cs
+++ Maze/Assets/LevelGenerator.cs
@@ -12,26 +12,34 @@ public class LevelGenerator : MonoBehaviour {
 	public GameObject wall;
 	public GameObject player;
 	public GameObject ground;
+	public Camera cam;
 
 	private bool playerSpawned = false;
 
 	// Use this for initialization
 	void Start () {
 		GenerateLevel();
+		player = GameObject.FindGameObjectWithTag("Player");
+		cam = Camera.main;
 
 		surface.BuildNavMesh();
 		total_ground++;
 	}
 
 	void Update () {
-		if (player.transform.position.x > ((total_ground-1)*22)-11)
+		var current_pos = player.transform.position;
+		if (current_pos.x >= ((total_ground-1)*22)-11)
 		{
 			Vector3 pos = new Vector3((total_ground*22f), 0f, 0f);
-			Instantiate(ground, pos, Quaternion.identity);
-			total_ground++;
+			Instantiate(ground, pos, Quaternion.identity, transform);
 			GenerateLevel();
 			surface.BuildNavMesh();
+			total_ground++;
 		}
+		var cam_pos = cam.transform.position;
+		cam_pos.x = cam_pos.x + (float)(0.01 + 0.01 * total_ground/5);
+		cam.transform.position = cam_pos;
+
 	}
 	
 	// Create a grid based level
@@ -43,10 +51,10 @@ public class LevelGenerator : MonoBehaviour {
 			for (int y = 0; y <= height; y+=2)
 			{
 				// Should we place a wall?
-				if (Random.value > .90f)
+				if (Random.value > .99f - (total_ground)/100)
 				{
 					// Spawn a wall
-					Vector3 pos = new Vector3(x - width / 2f, 1.5f, y - height / 2f);
+					Vector3 pos = new Vector3(x - width / 2f, 1.6f, y - height / 2f);
 					Instantiate(wall, pos, Quaternion.identity, transform);
 				} else if (!playerSpawned) // Should we spawn a player?
 				{
