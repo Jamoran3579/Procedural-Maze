99491eade3889c7537c82fdb1a238a077e01a3b8
Jamoran3579 <43176942+Jamoran3579@users.noreply.github.com>
2021-12-06 21:22:07 +0000

    Changes to allow ground to continually generate

--- Maze/Assets/LevelGenerator.cs
+++ Maze/Assets/LevelGenerator.cs
@@ -7,9 +7,11 @@ public class LevelGenerator : MonoBehaviour {
 
 	public int width = 10;
 	public int height = 10;
+	public int total_ground = 0;
 
 	public GameObject wall;
 	public GameObject player;
+	public GameObject ground;
 
 	private bool playerSpawned = false;
 
@@ -18,18 +20,30 @@ public class LevelGenerator : MonoBehaviour {
 		GenerateLevel();
 
 		surface.BuildNavMesh();
+		total_ground++;
+	}
+
+	void Update () {
+		if (player.transform.position.x > ((total_ground-1)*22)-11)
+		{
+			Vector3 pos = new Vector3((total_ground*22f), 0f, 0f);
+			Instantiate(ground, pos, Quaternion.identity);
+			total_ground++;
+			GenerateLevel();
+			surface.BuildNavMesh();
+		}
 	}
 	
 	// Create a grid based level
 	void GenerateLevel()
 	{
 		// Loop over the grid
-		for (int x = 0; x <= width; x+=2)
+		for (int x = total_ground*22; x <= width + (total_ground*22); x+=2)
 		{
 			for (int y = 0; y <= height; y+=2)
 			{
 				// Should we place a wall?
-				if (Random.value > .9f)
+				if (Random.value > .99f)
 				{
 					// Spawn a wall
 					Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
