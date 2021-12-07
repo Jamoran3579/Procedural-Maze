9a64c1ee2e57dcf8c7c962c8e0b9b594b5a3632e
Jamoran3579 <43176942+Jamoran3579@users.noreply.github.com>
2021-12-07 23:07:32 +0000

    Changes to make a game over screen

--- Maze/Assets/LevelGenerator.cs
+++ Maze/Assets/LevelGenerator.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class LevelGenerator : MonoBehaviour {
 
@@ -8,13 +9,16 @@ public class LevelGenerator : MonoBehaviour {
 	public int width = 10;
 	public int height = 10;
 	public int total_ground = 0;
-	public float difficulty = .99f;
+	public float difficulty = .95f;
 	private bool playerSpawned = false;
+	public bool gameover = false;
 
 	public GameObject wall;
 	public GameObject player;
 	public GameObject ground;
 	public Camera cam;
+	public Text score;
+	public Text result;
 
 	// Use this for initialization
 	void Start () {
@@ -27,23 +31,37 @@ public class LevelGenerator : MonoBehaviour {
 	}
 
 	void Update () {
-		var current_pos = player.transform.position;
-		if (current_pos.x >= ((total_ground-1)*22)-11)
+		if (gameover)
 		{
-			Vector3 pos = new Vector3((total_ground*22f), 0f, 0f);
-			Instantiate(ground, pos, Quaternion.identity, transform);
-			GenerateLevel();
-			surface.BuildNavMesh();
-			total_ground++;
-			if (difficulty > 0.7f)
-			{
-				difficulty = difficulty - 0.01f;
-			}
-		}
-		var cam_pos = cam.transform.position;
-		cam_pos.x = cam_pos.x + (float)(0.01 + 0.001 * total_ground);
-		cam.transform.position = cam_pos;
 
+		}
+		else
+		{
+			var current_pos = player.transform.position;
+			if (current_pos.x >= ((total_ground-1)*22)-11)
+			{
+				Vector3 pos = new Vector3((total_ground*22f), 0f, 0f);
+				Instantiate(ground, pos, Quaternion.identity, transform);
+				GenerateLevel();
+				surface.BuildNavMesh();
+				total_ground++;
+				if (difficulty > 0.75f)
+				{
+					difficulty = difficulty - 0.01f;
+				}
+			}
+			var cam_pos = cam.transform.position;
+			cam_pos.x = cam_pos.x + (float)(0.02 + 0.001 * total_ground);
+			cam.transform.position = cam_pos;
+			score.text = "Score: " + (total_ground - 2).ToString();
+
+			if (current_pos.x < cam_pos.x - 14)
+        	{
+				result.text = "Game Over\nYou scored: " + (total_ground - 2).ToString();
+        	    Application.Quit();
+				gameover = true;
+        	}
+		}
 	}
 	
 	// Create a grid based level
--- Maze/Assets/PlayerController.cs
+++ Maze/Assets/PlayerController.cs
@@ -4,14 +4,16 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool gameover = false;
+
     public Camera cam;
-
     public NavMeshAgent agent;
-
     public ThirdPersonCharacter character;
+    public GameObject player;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         agent = GetComponent<NavMeshAgent>();
         cam = Camera.main;
     
@@ -21,26 +23,43 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        if (gameover)
+		{
 
-            if (Physics.Raycast(ray, out hit))
+		}
+		else
+		{
+            var current_pos = player.transform.position;
+            var cam_pos = cam.transform.position;
+            
+            if (Input.GetMouseButtonDown(0))
             {
-                agent.SetDestination(hit.point);
-            }
-        }
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
 
-        if (agent.remainingDistance > agent.stoppingDistance)
-        {
-            character.Move(agent.desiredVelocity, false, false);
-        } else
-        {
-            character.Move(Vector3.zero, false, false);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+            var speed = agent.speed;
+		    speed = speed + (float)0.0001;
+		    agent.speed = speed;
+
+            if (current_pos.x < cam_pos.x - 14)
+        	{
+        	    Application.Quit();
+				gameover = true;
+        	}
         }
-        var speed = agent.speed;
-		speed = speed + (float)0.0001;
-		agent.speed = speed;
     }
 }
